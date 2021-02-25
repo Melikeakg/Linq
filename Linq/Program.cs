105abe08a11a2964dd65e7e476369010cd8073a4
Melikeakg <79082564+Melikeakg@users.noreply.github.com>
2021-02-25 14:16:22 +0300

    Linq ve join kullanımı

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -8,6 +8,11 @@ namespace Linq
     {
         static void Main(string[] args)
         {
+            List<Category> categories = new List<Category>
+            {
+                new Category{ CategoryId=1, CategoryName="Bilgisayar"},
+                new Category{ CategoryId=2, CategoryName="Telefon"}
+            };
             List<Product> products = new List<Product>
             {
                 new Product{ ProductId=1, CategoryId=1, ProductName="Acer Laptop", QuantityPerUnit="32 GB RAM", UnitPrice=1000, UnitsInStock=5 },
@@ -31,7 +36,35 @@ namespace Linq
             {
                 Console.WriteLine(product.ProductName);
             }
+
+            var result5 = from p in products
+                          where p.UnitPrice > 6000
+                          orderby p.UnitPrice descending, p.ProductName ascending
+                          select p;
+
+            foreach (var product in result5)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            var result6 = from p in products
+                          join c in categories
+       on p.CategoryId equals c.CategoryId
+                          select new ProductDto { ProductId = p.ProductId, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+
+            foreach (var productDto in result6)
+            {
+                Console.WriteLine(productDto.ProductName + " " + productDto.CategoryName);
+            }
         }
+    }
+
+    class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
     }
 
     class Product
@@ -43,5 +76,11 @@ namespace Linq
         public int UnitPrice { get; set; }
         public int UnitsInStock { get; set; }
 
+    }
+
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
     }
 }
